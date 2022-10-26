8f8e00749b8d942d3d4ef1aa49a6c3d5460efcdd
Zainal aAbiddin <zabiddin6@gmail.com>
2022-10-26 08:50:44 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "Hello world! <br>";
+
+    $nama = "Zainal Abiddin";
+    $usia = 21;
+
+    echo "nama saya <strong>$nama</strong>,
+    saya berusia $usia tahun.<br>";
+
+    $namaAdik = "Olivia Syalsabella";
+    $usiaAdik = "20";
+
+    echo "nama adik saya <strong>$namaAdik</strong>,
+    Adik saya berusia $usiaAdik tahun.<br>";
+
+    $selisihUsia = $usia - $usiaAdik;
+
+    echo "Selisih usia saya dan adik saya adalah $selisihUsia tahun.";
+
+?>
